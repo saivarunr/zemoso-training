2191fd55eefd89bda5bdbcf971d0233ea82b2bc0
saivarun <saivarun.r@zemosolabs.com>
2016-07-08 16:44:35 +0530

    new functions are added to minimize the radius

--- drawCircles.js
+++ drawCircles.js
@@ -1,6 +1,7 @@
 /**
  * @author varun
  */
+
 var xcon=[]; //X-Axis co-ordinate container
 var ycon=[]; //Y-axis co-ordinate container
 var index=0;	//index used for adding "ids" to points in the place
@@ -15,7 +16,7 @@ var fx1,fy1,fx2,fy2; /*
                       */
 var radius; 		//radius
 function display(x,y,id,r){
-	//Function which displays a point or a circle with their id and radius
+	//Function which displays a point or a circle with their id and radius	
 	d3.select("#circleAdder").append("circle").attr("cx",x).attr("cy",y).attr("r",r).style("fill","none").attr("stroke","black").attr("id",id);
 }
 function drawCircle(x,y){
@@ -44,7 +45,7 @@ function drawCircle(x,y){
 		}
 		
 		//Call to this method to get new co-ordinates of center and radius
-		var C=getNewCenter(cx,cy,x,y,fx,fy);
+		var C=getNewCenter(cx,cy,x,y,x,y,fx,fy);
 		cx=C[0];
 		cy=C[1];
 		radius=C[2];
@@ -67,16 +68,64 @@ function drawCircle(x,y){
 			drawCircle(xcon[i],ycon[i]);
 		}
 	}
+	
 }
 
+/*
+ * This function takes two points A,B on the circle and tries to minimize the radius if
+ * the points are not diametrically opposite (Angle of 180 degrees between two points),
+ * it tries to move the Center(cx,cy) to the farthest point available in the circle except A,B
+ */
+function minimize(){
+	//If difference is 180 degrees (equivalent numerical value is 3.14) return true
+	if(Math.abs(Math.atan2(fy1-cy,fx1-cx)-Math.atan2(fy2-cy,fx2-cx))==Math.PI)
+		return true;
+	//If not
+	//Get the farthest co-ordinates from the center of the circle
+	coordinates=getFarthest(cx,cy);		
+	//Calculate the new center X, Center Y and radius
+	Center=getNewCenter(cx,cy,(fx1+fx2)/2,(fy1+fy2)/2,coordinates[0],coordinates[1],fx1,fy1);
+	cx=Center[0];
+	cy=Center[1];
+	radius=Center[2];
+	//remove the previous circle containing points
+	d3.select("#big").remove();
+	//Update the new circle
+	display(cx,cy,"big",radius);
+}
+
+
+//This function returns farthest co-ordinates in a circle from (x,y) which aren't points on the circle i.e A,B
+function getFarthest(x,y){
+	var farthest=-1;
+	var coordinates=[];
+	var l=xcon.length;
+	for(var i=0;i<l;i++){
+		if((xcon[i]!=fx1&&ycon[i]!=fy1)&&(xcon[i]!=fx2&&ycon[i]!=fy2)){
+			if(getDistance(xcon[i],ycon[i],x,y)>farthest){
+				farthest=getDistance(xcon[i],ycon[i],x,y);
+				coordinates=[xcon[i],ycon[i]];
+			}
+		}
+	}
+return coordinates;
+}
 
 function getDistance(x1,y1,x2,y2){
 //returns distance between two points
 	return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
 }
 
-
-function getNewCenter(cx,cy,tx,ty,fx,fy){
+/*
+ * This function calculates new center (newx,newy) by taking old center (cx,cy) and the target point
+ * to where the center (cx,cy) to be moved (tx,ty) and 
+ * two reference fixed points (fx1,fy1) (fx2,fy2)
+ * these two reference points serve as reference, 
+ * 
+ *------------------------------------------
+ * A new center is found when distance between (newx,newy)-(fx1,fy1) is equal to (newx,newy)-(fx2,fy2)
+ */
+function getNewCenter(cx,cy,tx,ty,fx1,fy1,fx2,fy2){
 	//Returns new center co-ordinates and radius
 	var dx=tx-cx;
 	var dy=ty-cy;
@@ -90,7 +139,7 @@ function getNewCenter(cx,cy,tx,ty,fx,fy){
 	if(cx>tx){
 		for(var x=cx;x>tx;x--){
 			y=cy+dy*(x-cx)/dx;
-			var temp=Math.abs(Number.parseInt(getDistance(x,y,tx,ty)-getDistance(x,y,fx,fy)));
+			var temp=Math.abs(Number.parseInt(getDistance(x,y,fx1,fy1)-getDistance(x,y,fx2,fy2)));
 			if(m>temp){
 				m=temp;
 				newx=x;
@@ -102,7 +151,7 @@ function getNewCenter(cx,cy,tx,ty,fx,fy){
 else{
 		for(var x=cx;x<tx;x++){
 			y=cy+dy*(x-cx)/dx;
-			var temp=Math.abs(Number.parseInt(getDistance(x,y,tx,ty)-getDistance(x,y,fx,fy)));
+			var temp=Math.abs(Number.parseInt(getDistance(x,y,fx1,fy1)-getDistance(x,y,fx2,fy2)));
 			if(m>temp){
 				m=temp;
 				newx=x;
@@ -111,6 +160,6 @@ else{
 	}
 }
 //Return maximum radius of target point and fixed point (the point which was farthest point on circle from target point)
-var radius=Math.max(getDistance(newx,newy,tx,ty),getDistance(newx,newy,fx,fy));
+var radius=Math.max(getDistance(newx,newy,fx1,fy1),getDistance(newx,newy,fx2,fy2));
 return [newx,newy,radius];
 }
